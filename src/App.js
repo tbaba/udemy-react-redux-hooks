306ec4d1ee54f7d9b5115aa6fb1fdae4bf44204f
Tatsuro Baba <harakirisoul@gmail.com>
2020-05-17 18:45:16 +0900

    useState with one object state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { useState } from 'react';
 
 const App = props => {
-  const initialState = props;
-
-  const [name, setName] = useState(initialState.name);
-  const [price, setPrice] = useState(initialState.price);
-
-  const reset = () => {
-    setName(initialState.name);
-    setPrice(initialState.price);
-  }
+  const [state, setState] = useState(props);
+  const { name, price } = state;
 
   return (
     <>
       <p>現在の{name}は{price}円です。</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input type="text" onChange={e => setName(e.target.value)} value={name} />
+      <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input type="text" onChange={e => setState({ ...state, name: e.target.value })} value={name} />
     </>
   );
 };
