2c9890849e131fe5ef57bf1710936eead8fe2fd4
Tatsuro Baba <harakirisoul@gmail.com>
2020-05-17 21:03:46 +0900

    Enable to save new event

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
+import reducer from '../reducers';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [event, setEvent] = useState({});
+
+  const addEvent = (e) => {
+    e.preventDefault();
+
+    dispatch({ type: 'CREATE_EVENT', ...event });
+    return setEvent({ title: '', body: '' });
+  };
+
+  const changeEvent = (value, name) => {
+    switch(name) {
+      case 'title':
+        return setEvent({ ...event, title: value });
+      case 'body':
+        return setEvent({ ...event, body: value });
+      default:
+        return event;
+    }
+  }
+
   return (
     <div className="container-fluid">
       <div className="newEventForm mb-5">
@@ -9,13 +31,23 @@ const App = () => {
         <form>
           <div className="form-group">
             <label htmlFor="formEventTitle">タイトル</label>
-            <input className="form-control" id="formEventTitle" />
+            <input
+              className="form-control"
+              id="formEventTitle"
+              onChange={e => changeEvent(e.target.value, 'title')}
+              value={event.title}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="formEventBody">ボディー</label>
-            <textarea className="form-control" id="formEventBody" />
+            <textarea
+              className="form-control"
+              id="formEventBody"
+              onChange={e => changeEvent(e.target.value, 'body')}
+              value={event.body}
+            />
           </div>
-          <button className="btn btn-primary">イベントを作成する</button>
+          <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
           <button className="btn btn-danger">全てのイベントを削除する</button>
         </form>
       </div>
