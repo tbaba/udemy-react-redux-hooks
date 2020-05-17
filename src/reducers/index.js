0d710126d8a5bc0b30eb7f63143edc31cfc9c57f
Tatsuro Baba <harakirisoul@gmail.com>
2020-05-17 20:37:30 +0900

    [wip] Create reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,31 @@
+// action = {
+//   type: 'CREATE_EVENT',
+//   title: '新しいイベント',
+//   body: '新しいイベントです',
+// }
+//
+// state = []
+// state = [
+//   { id: 1, title: 'ほげ', body: 'ふが' },
+//   { id: 2, title: 'ふー', body: 'ばー' },
+//   { id: 3, title: 'ほげ', body: 'ぴよ' },
+// ]
+const events = (state = [], action) => {
+  switch(action.type) {
+    case 'CREATE_EVENT':
+      const { title, body } = action;
+      const event = { title, body };
+      const length = state.length;
+      const id = length === 0 ? 1 : state[length -1].id + 1;
+
+      return [...state, { id, ...event }];
+    case 'DELETE_EVENT':
+      return state;
+    case 'DELETE_ALL_EVENTS':
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default events;
